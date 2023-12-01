c4a9ba1fab0448ba8fb9dcdec1bb93168891e9b9
Пользователь <dashyamoiseenko2005@gmail.com>
2023-12-01 14:30:36 +0300

    pr

--- pr9/блок б номер 2.py
+++ pr9/блок б номер 2.py
@@ -1,4 +1,17 @@
-a='девятая практическая работа'
-b=a.split()
-c=''.join([''.join(sorted(a)) for a in b])
-print(c)
+def f():
+    z1 = z2 = 0    
+    def n(z1, z2):
+        c = int(input("Введите натуральное число: "))        
+        if c == 0:
+            return z2
+        if c > z1:
+            return n(c, z1)
+        elif c > z2:
+            return n(z1, c)
+        else:
+            return n(z1, z2)
+    r = n(z1, z2)
+    return r
+
+r = f()
+print("Второе по величине число:",r)
